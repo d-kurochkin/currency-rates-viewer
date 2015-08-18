1b91b4e8a54edf325c9743ddf8c12e5ef5fdac8a
Dmitriy Kurochkin <dm.v.kurochkin@gmail.com>
2015-08-18 17:29:46 +0600

    Add test_get_pairs_USD

--- tests/test_xchange.py
+++ tests/test_xchange.py
@@ -4,7 +4,14 @@ import xchange
 
 class TestXchange(TestCase):
     def test_get_pairs_unknown(self):
-        self.fail()
+        self.assertRaises(ValueError, xchange.get_ratios, "DS")
+
+    def test_get_pairs_USD(self):
+        pairs = ['USDAUD', 'USDCAD', 'USDCHF', 'USDCNY', 'USDEUR', 'USDGBP',
+                 'USDGPY', 'USDKZT', 'USDRUB', 'USDUAH']
+        result = xchange.get_pairs("USD")
+
+        self.assertEqual(pairs, result)
 
     def test_get_ratio_unknown(self):
         self.assertRaises(ValueError, xchange.get_ratios, "DS")
