73f8271bdff80cd3422aff9f4a4d816f34a05c27
Dmitriy Kurochkin <dm.v.kurochkin@gmail.com>
2015-08-19 11:17:47 +0600

    Test for test_convert_arguments_to_hash

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
 from cache import Cached
 
+
 class TestCache(TestCase):
     def test_convert_arguments_to_hash(self):
-        pass
+        def test_fn(*args, **kwargs):
+            return Cached._convert_arguments_to_hash(args, kwargs)
+
+        args_hash = hash("(1, 'foo'){'name': 'name'}")
+        result = test_fn(1, 'foo', name='name')
+
+        self.assertEqual(args_hash, result)
