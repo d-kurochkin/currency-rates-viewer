868c9d8b5ad897a49109dbb4e675c2d6209ea372
Dmitriy Kurochkin <dm.v.kurochkin@gmail.com>
2015-08-19 15:53:03 +0600

    Implement behavior for CurrencySelector

--- static/app.js
+++ static/app.js
@@ -1,28 +1,25 @@
 var CurrencySelector = React.createClass({
     getInitialState: function () {
-        return {currencies: []};
+        return {value: 'select'}
     },
 
-    componentDidMount: function () {
-        var context = this;
+    change: function (event) {
+        var value = event.target.value;
 
-        //Load all available currencies from server
-        fetch('/list').then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            context.setState(data);
-        });
+        jupiter('select').pub(value);
+        this.setState({value: value});
     },
 
     render: function () {
-        var currencies = this.state.currencies;
+        var currencies = this.props.currencies;
 
         return (
             <section id="currency-selector">
-                <select placeholder="add">
-                    <option selected>Add</option>
+                <span>Base currency</span>
+                <select placeholder="add" value={this.state.value} onChange={this.change}>
+                    <option value="select">Select</option>
                     {currencies.map(function (item) {
-                        return <option value="{item}">{item}</option>
+                        return <option value={item}>{item}</option>
                     })}
                 </select>
             </section>
@@ -31,39 +28,55 @@ var CurrencySelector = React.createClass({
 });
 
 var CurrencyItem = React.createClass({
-    handleClick: function () {
-        jupiter("currencyClick").pub(this.props.name)
-    },
-
     render: function () {
         return (
             <li>
                 <span>{this.props.name}</span> &nbsp;
                 <span>{this.props.ratio}</span> &nbsp;
-                <button onClick={this.handleClick}>x</button>
             </li>
         )
     }
 });
 
-var Main = React.createClass({
+var MainComponent = React.createClass({
+    getInitialState: function () {
+        return {
+            main: '',
+            currencies: [],
+            rates: []
+        }
+    },
+
+    componentDidMount: function () {
+        //Save context to use setState
+        var main = this;
+
+        fetch('/list').then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            main.setState(data);
+        });
+    },
+
     render: function () {
+        var currencies = this.state.currencies;
+
         return (
             <div>
-                <CurrencySelector />
+                <CurrencySelector currencies={currencies}/>
 
                 <ul id="currency-list">
-                    <CurrencyItem name="USD" ratio="0.5"/>
-                    <CurrencyItem name="KZT" ratio="0.7"/>
-                    <CurrencyItem name="UAH" ratio="0.9"/>
+                    {currencies.map(function (item) {
+                        return <CurrencyItem name={item} ratio="0"/>
+                    })}
                 </ul>
             </div>
         );
     }
 });
 
-React.render(<Main />, document.getElementById("main"));
+var MainComponentMounted = React.render(<MainComponent />, document.getElementById("main"));
 
-jupiter("currencyClick").sub(function (name) {
-    console.log('Click on ' + name)
+jupiter('select').sub(function (arg) {
+    console.log('Selected item ' + arg)
 });
