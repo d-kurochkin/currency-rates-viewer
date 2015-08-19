bc17e14e8434de4e85e6eea34e3d14014fa1e708
Dmitriy Kurochkin <dm.v.kurochkin@gmail.com>
2015-08-19 11:08:30 +0600

    Implement Cached stub and add tests

--- cache.py
+++ cache.py
@@ -1,1 +1,8 @@
+class Cached(object):
+    def __init__(self, lifetime=60):
+        self.lifetime = lifetime
+        self.cache_data = {}
 
+    @staticmethod
+    def _convert_arguments_to_hash(args, kwargs):
+        return hash(str(args) + str(kwargs))
---
+++ tests/test_cache.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+from cache import Cached
+
+class TestCache(TestCase):
+    def test_convert_arguments_to_hash(self):
+        pass
